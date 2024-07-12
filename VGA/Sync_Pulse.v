f04de771838921b084c724df36bd8184b339c84a
DylanxCS <dylan.swansond@gmail.com>
2024-07-12 15:23:42 -0500

    Save

--- VGA/Sync_Pulse.v
+++ VGA/Sync_Pulse.v
@@ -1,6 +1,6 @@
 module Sync_Pulse(
 input CLK,
-output H_pulse, V_pulse
+output H_Sync, V_Sync
 );
 
 // 800 Col by 525 Row
@@ -12,38 +12,38 @@ output H_pulse, V_pulse
 // 25Mhz / 525 = 47619.0476 Clock cycles per row
 
 // H-Sync = High for 25000 Clock cycles
-// V-Sync = High for 43537.4149 Clock cycles 
+// V-Sync = High for 43537.4149 Clock cycles <-- UPDATE
 
 reg [9:0] r_CountCol = 0;
 reg [9:0] r_CountRow = 0;
-reg r_H_pulse = 1'b1;
-reg r_V_pulse = 1'b1;
+reg r_H_Sync = 1'b1;
+reg r_V_Sync = 1'b1;
 
 always @(posedge CLK)
 begin
    if (r_CountCol < 640)// maybe this should be 38400
-    r_H_pulse <= 1'b1;
+    r_H_Sync <= 1'b1;
   else if (r_CountCol < 800)// maybe this should be 48000
-    r_H_pulse <= 1'b0;
+    r_H_Sync <= 1'b0;
   else begin
     r_CountCol <= 0;
-    r_H_pulse <= 1'b1;
+    r_H_Sync <= 1'b1;
   end
   r_CountCol <= r_CountCol + 1;
   
   if (r_CountRow < 480)// maybe this should be 28800
-    r_V_pulse <= 1'b1;
+    r_V_Sync <= 1'b1;
   else if (r_CountRow < 525)// maybe this should be 31500
-    r_V_pulse <= 1'b0;
+    r_V_Sync <= 1'b0;
   else begin
     r_CountRow <= 0;
-    r_V_pulse <= 1'b1;
+    r_V_Sync <= 1'b1;
   end
     
   r_CountRow <= r_CountRow + 1;
 end
 
-assign H_pulse = r_H_pulse;
-assign V_pulse = r_V_pulse;
+assign H_Sync = r_H_Sync;
+assign V_Sync = r_V_Sync;
 
 endmodule
